4e6ae82298f44174a7126d1d2ca69cb5aa50a3bb
Asher Freeland <freelash000@alpinesd.org>
2025-11-04 09:07:40 -0700

    Guess the number

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const btnSubmit = document.getElementById("btnSubmit");
+/* const btnSubmit = document.getElementById("btnSubmit");
 const pText = document.getElementById("pText");
 const inputSubmit = document.querySelector(".inputSubmit");
 
@@ -11,4 +11,26 @@ function displayText() {
   pText.style.fontWeight = "bold";
 
   pText.innerHTML = inputValue;
+}
+
+let name = prompt("What is your name?");
+ alert(`Hi ${name}, how are you?`); 
+let color = prompt("What is your favortie color?");
+if (color === "blue") {
+  alert(`That is my favorite color too!`);
+} else {
+  alert(`That is a nice color!`);
+} */
+
+const numberInp = document.querySelector("inputSubmit");
+const guessBtn = document.querySelector("btnGuess");
+const attemptConst = document.querySelector("attemptSpan");
+
+let secretNumber = Math.floor(Math.random() * 10) + 1;
+guessBTn.addEventListener("click", onGuess);
+function onGuess() {
+  attemptConst++;
+  const inputValue = Number(numberInp.value);
+}
+if (secretNum === inputValue) {
 }
