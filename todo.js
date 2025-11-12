bfa986ebe99149e4895b52a7b5e6acf2019507c1
Asher Freeland <freelash000@alpinesd.org>
2025-11-12 08:54:00 -0700

    Todo List

--- todo.js
+++ todo.js
@@ -1,32 +1,92 @@
 const addBtn = document.querySelector(".addBtn");
 const listInput = document.querySelector(".listInput");
 const tasksList = document.querySelector(".todoList");
-const deleteBtn = document.querySelector(".deleteBtn");
+// const deleteBtn = document.querySelector(".deleteBtn");
 
 /* deleteBtn.addEventListener('click', () =>; {
 
-});
+}); */
 
-addBtn.addEventListener('click', addTask{
-    
-}; */
+addBtn.addEventListener("click", addTask);
 
 let listitems = [];
 
-function showItem(listItem) {
-  listitems.push(listItem);
-}
-for (let i = 0; i < listitems.length; i++) {
-  tasksList.appendChild(listitems[i]);
+function deleteItem(event) {
+  const buttonClicked = event.target;
+  buttonClicked.closet("li").remove();
 }
 
 function addTask() {
-  const inpValue = addInput.value.trim();
+  let inpValue = listInput.value.trim();
 
-  if (impValue !== "") {
+  if (inpValue !== "") {
     const listItem = document.createElement("li");
-    listItem.innerHTML = inpValue;
+    listItem.textContent = inpValue;
+    showItem(listItem);
+    listInput.value = "";
   } else {
     alert("Task required");
   }
 }
+function showItem(listItem) {
+  listitems.push(listItem);
+
+  for (let i = 0; i < listitems.length; i++) {
+    tasksList.appendChild(listitems[i]);
+
+    const taskDeleteBtn = document.createElement("button");
+    taskDeleteBtn.textContent = "Delete";
+    taskDeleteBtn.classList.add("deleteBtn");
+
+    taskDeleteBtn.addEventListener("click", deleteItem);
+
+    tasksList.appendChild(taskDeleteBtn);
+
+    tasksList.appendChild(tasksList);
+  }
+  listInput.value = "";
+}
+
+//   // ...existing code...
+// const addBtn = document.querySelector(".addBtn");
+// const listInput = document.querySelector(".listInput");
+// const tasksList = document.querySelector(".todoList");
+// // ...existing code...
+
+// addBtn.addEventListener("click", addTask);
+
+// let listitems = [];
+
+// function deleteItem(event) {
+//   const buttonClicked = event.target;
+//   const li = buttonClicked.closest("li");
+//   if (li) li.remove();
+// }
+
+// function addTask() {
+//   let inpValue = listInput.value.trim();
+
+//   if (inpValue !== "") {
+//     const listItem = document.createElement("li");
+//     listItem.textContent = inpValue;
+//     showItem(listItem);
+//     listInput.value = "";
+//   } else {
+//     alert("Task required");
+//   }
+// }
+
+// function showItem(listItem) {
+//   // append delete button into the li and then append the li once
+//   const taskDeleteBtn = document.createElement("button");
+//   taskDeleteBtn.textContent = "Delete";
+//   taskDeleteBtn.classList.add("deleteBtn");
+//   taskDeleteBtn.addEventListener("click", deleteItem);
+
+//   listItem.appendChild(taskDeleteBtn);
+//   tasksList.appendChild(listItem);
+
+//   listitems.push(listItem);
+//   // no looping over listitems and no appending tasksList to itself
+// }
+// // ...existing code...
